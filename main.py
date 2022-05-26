1a0d36df6b8f58cda0e4cddfadfe87f46bae4770
aposum23 <81620056+aposum23@users.noreply.github.com>
2022-05-26 03:11:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from threading import Thread
 from time import sleep
+import math
 
-def solve():
-    x = 2
-    res = []
-    for i in range(10000000000):
-        res.append(x ** 3 / (1 * 2 * 3) + x ** 5 / (1 * 2 * 3 * 4 * 5))
-        print(f'sum: {sum(res)}')
+eps = 10 ** (-7)
+
+def solve(x, num):
+    global eps
+    a = 1
+    summa = 1
+    i = 1
+    prev = 0
+    y = (eps ** x - eps ** (-x)) / 2
+    while abs(summa - prev) > eps:
+        a = x ** 3 / (1 * 2 * 3) + x ** 5 / (1 * 2 * 3 * 4 * 5)
+        prev = summa
+        if i % 2 == 0:
+            summa += a
+        else:
+            summa += -1 * a
+        i += 1
+    print(f"Sum: {num} = {summa}")
+    print(f"Check: {y}")
 
 
 if __name__ == '__main__':
-    th = Thread(target=solve)
+    th = Thread(target=solve, args=(0, 1))
     th.start()
     solve()
     x = 2
-    eps = 10 ** (-7)
-    y = (eps ** x - eps ** (-x)) / 2
-    print(f'y: {y}')
